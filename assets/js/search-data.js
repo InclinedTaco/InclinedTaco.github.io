e6e23272936fb5379d5f09bd929bc2db04af265d
InclinedTaco <103011780+InclinedTaco@users.noreply.github.com>
2025-11-15 17:26:03 +0000

    Deploying to gh-pages from @ InclinedTaco/InclinedTaco.github.io@f6f41504a25ec765ea3e2d426bfb77df0c8bc111 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,8 +398,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-joined-robotics-and-machine-intelligence-as-robotics-engineer",
+          title: 'Joined Robotics and Machine Intelligence as Robotics Engineer.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
